1583bafaa0a779220bbe1232b8c32ac41e825399
HonorGamerHD <niebuhrsteve@gmail.com>
2018-08-22 21:25:26 +0200

    Fixed MeCommand

--- src/MeCommand/MeCommand.php
+++ src/MeCommand/MeCommand.php
@@ -13,16 +13,20 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as SH;
 
 class MeCommand extends PluginBase{
     public function onEnable()
     {
+    	@mkdir($this->getDataFolder());
+    	$this->saveResource("Config.yml");
         $this->Banner();
         $this->getLogger()->info(SH::GREEN . "Server-Helper was activated!");
     }
 
     public function onDisable(){
+    	$this->saveResource("Config.yml");
         $this->Banner();
         $this->getLogger()->info("Server-Helper was stopped!");
     }
@@ -43,12 +47,13 @@ class MeCommand extends PluginBase{
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
+    	$this->myConfig = new Config($this->getDataFolder() . "Config.yml", Config::YAML);
         switch ($command->getName()){
             case "me":
                 if($sender instanceof Player){
                     if($sender->hasPermission("mecommand.command.me")){
                         if(!empty($args[0])){
-                            $sender->getServer()->broadcastMessage("* " . $sender->getName() . " " . $args[0]);
+                        	$sender->getServer()->broadcastMessage($this->getConfig()->get("MePrefix") . SH::RESET . " ". $this->getConfig()->get("ColourName") . $sender->getName() . SH::RESET . " " . $this->getConfig()->get("ColourMSG") . implode(" ", $args));
                         }else{
                             $sender->sendMessage("Please specify a Message!");
                         }
